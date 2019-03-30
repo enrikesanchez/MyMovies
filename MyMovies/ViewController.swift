167a18f255614750b7c33a71fa7fafb5ccb2e890
user148189 <user148189@74-80-245-25.ic2net.net>
2019-03-30 10:44:32 -0700

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by user148189 on 3/30/19.
+//  Copyright © 2019 Tec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
